5e415d2277cb85d403b8329dca6a7882d7472d63
fan_c <2207771247@qq.com>
2023-11-12 17:09:36 +0800

    test-c

--- yaka/vite.config.js
+++ yaka/vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-c/',
+  // base: '/test-c/',
+  base:'./',
   plugins: [
     vue(),
   ],
