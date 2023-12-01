8538237bcaaa3b1c22b3abf34d7409eff3e1dbd3
fan0607 <89071246+fan0607@users.noreply.github.com>
2023-12-01 16:37:00 +0800

    Delete editor directory

--- editor/src/views/ObjectEditor/glsl/flowLights/vertexShader.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-varying vec3 fNormal;
-varying vec3 vPosition;
-void main(){
-    vUv=uv;
-    vPosition=position;
-    vec4 mvPosition=modelViewMatrix*vec4(position,1.);
-    gl_Position=projectionMatrix*mvPosition;
-}
--- editor/src/views/ObjectEditor/utils/events.ts
+++
@@ -1,7 +0,0 @@
-export default function onWindowResize(camera:THREE.PerspectiveCamera, renderer:THREE.Renderer, threeOceanWidth:number, threeOceanHeight:number) {
-
-    camera.aspect = threeOceanWidth / threeOceanHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(threeOceanWidth, threeOceanHeight);
-}
