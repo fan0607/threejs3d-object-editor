d983361525e58192bfb9c06cc16227b38fcc0a99
fan0607 <89071246+fan0607@users.noreply.github.com>
2023-12-11 16:37:55 +0800

    Delete gridEditor directory

--- gridEditor/electron-vite-project/src/views/GridPlane/utils/events.js
+++
@@ -1,7 +0,0 @@
-export default function onWindowResize(camera, renderer, threeOceanWidth, threeOceanHeight) {
-
-    camera.aspect = threeOceanWidth / threeOceanHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(threeOceanWidth, threeOceanHeight);
-}
